8a80018e881477dea0712b35173238d52ed1893e
Azazlokus <azku@inbox.ru>
2025-03-15 01:17:17 +0700

    fix: Задание 3. Исправлена ошибка с планировщиком задач. Добавлено логирование.

--- task-manager/app/Jobs/AssignUnassignedTasksJob.php
+++ task-manager/app/Jobs/AssignUnassignedTasksJob.php
@@ -5,31 +5,30 @@ namespace App\Jobs;
 use App\Models\Task;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
-class AssignTasksJob implements ShouldQueue
+class AssignUnassignedTasksJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Execute the job.
-     */
     public function handle(): void
     {
+        Log::info('AssignUnassignedTasksJob запустился');
         $tasks = Task::doesntHave('users')->get();
-        $users = User::where('status', 'active')->get();
+        $users = User::inRandomOrder()->get();
 
-        if ($users->isEmpty()) {
+        if ($tasks->isEmpty() || $users->isEmpty()) {
             return;
         }
 
         foreach ($tasks as $task) {
             $randomUser = $users->random();
             $task->users()->attach($randomUser->id);
+            Log::info("Задача ID {$task->id} назначена пользователю ID {$randomUser->id}");
         }
     }
 }
--- task-manager/routes/console.php
+++ task-manager/routes/console.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Console\ClosureCommand;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Jobs\AssignUnassignedTasksJob;
 
-Artisan::command('inspire', function () {
-    /** @var ClosureCommand $this */
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+
+Schedule::job(new AssignUnassignedTasksJob())->everyTwoMinutes();
